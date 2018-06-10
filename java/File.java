83689ee9b90034dfe90283c34e488ba2bbf091fc
alexitosrv <alexander.salamanca@gmail.com>
2018-06-09 22:42:16 -0500

    added common imports

--- java/File.java
+++ java/File.java
@@ -1,7 +1,22 @@
+// here it goes the import statements
 
-
-public class File{
+public class File// here the string File represents the name of the class, ofc is the same name of the file with the .java extension
+{
 	
 	
 	
 }
+import java.io.Console;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeSet;
